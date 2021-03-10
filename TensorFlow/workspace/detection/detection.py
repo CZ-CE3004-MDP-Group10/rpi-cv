6dc28ea7a3aa0f7d2130daf999daf6a367dc9412
KarenWongkx <44762153+KarenWongkx@users.noreply.github.com>
2021-03-10 17:24:48 +0800

    coordinate check

--- TensorFlow/workspace/detection/detection.py
+++ TensorFlow/workspace/detection/detection.py
@@ -1,3 +1,4 @@
+import sys
 import cv2
 import os
 import socket
@@ -9,33 +10,25 @@ from PIL import Image
 from object_detection.builders import model_builder
 from object_detection.utils import label_map_util, config_util
 from object_detection.utils import visualization_utils as viz_utils
+from multiprocessing import Process
 
 # Path variables
 MODEL_DATA_PATH = "./model"
 CONFIG_PATH = MODEL_DATA_PATH + "/pipeline.config"
 CHECKPOINT_PATH = MODEL_DATA_PATH + "/"
 category_index = label_map_util.create_category_index_from_labelmap(MODEL_DATA_PATH + '/label_map.pbtxt')
-IMAGES_PATH = "./images"
+RAW_IMAGES_PATH = "./images"
+INFERED_IMAGES_PATH = "./images/infered"
 
 # Variables
 HOST = "192.168.10.1"
 PORT = 50001
 MIN_THRESHOLD = .80
 MAX_BOUNDING_BOXES = 100
-
-# image_detect_q = Queue()
-
-# Load pipeline config and build a detection model
-configs = config_util.get_configs_from_pipeline_file(CONFIG_PATH)
-detection_model = model_builder.build(model_config=configs['model'], is_training=False)
-# Restore checkpoint
-ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
-ckpt.restore(os.path.join(CHECKPOINT_PATH, 'ckpt-23')).expect_partial()
-
-
-# If read images in folder
-def load_image_into_numpy_array(path):
-    return np.array(Image.open(path))
+MIN_X = 0.4
+MAX_X = 0.6
+MIN_Y = 0.6
+MAX_Y = 0.8
 
 
 def convert_image(received_image):
@@ -74,6 +67,13 @@ def process_detections(input_tensor):
     return detections
 
 
+def get_center_of_label(coordinates):
+    y_min, x_min, y_max, x_max = coordinates
+    mid_x = (x_min + x_max) / 2
+    mid_y = (y_min + y_max) / 2
+    return mid_x, mid_y
+
+
 def detection_information(detections):
     individual_detection = []
     classes = detections['detection_classes']
@@ -83,11 +83,14 @@ def detection_information(detections):
     for i in range(min(MAX_BOUNDING_BOXES, boxes.shape[0])):
         if scores is None or scores[i] > MIN_THRESHOLD:
             if classes[i] + 1 in category_index.keys():
-                class_name = category_index[classes[i] + 1]
                 coordinates = tuple(boxes[i].tolist())  # [y_min, x_min, y_max, x_max]
-                print(class_name, coordinates)
-                individual_detection.append(class_name)
-    print(individual_detection)
+                mid_x, mid_y = get_center_of_label(coordinates)
+                # Check coordinates location
+                if MIN_X <= mid_x <= MAX_X and MIN_Y <= mid_y <= MAX_Y:
+                    class_name = category_index[classes[i] + 1]["name"]
+                    label = [class_name, scores[i], mid_x, mid_y]
+                    print(label)
+                    individual_detection.append(label)
     return individual_detection
 
 
@@ -105,9 +108,8 @@ def show_inference(image_np_with_detections, detections):
         min_score_thresh=MIN_THRESHOLD,
         line_thickness=1,
         agnostic_mode=False)
-
     write_image_time = time.time()
-    cv2.imwrite("./output_images/" + "file_name" + str(image_count) + ".jpg", image_np_with_detections)
+    cv2.imwrite(f"{INFERED_IMAGES_PATH}/processed_image_{image_count}.jpg", image_np_with_detections)
     print("--- %s seconds <time taken to write image to folder> ---" % (time.time() - write_image_time))
 
 
@@ -118,140 +120,76 @@ def run_inference_for_single_image(received_image):
     detected_items = detection_information(detections)
     # To get the image with bounding boxes
     if detected_items is not None:
+        # sbuf.put_utf8("detected label" + str(detected_items[0]))
         show_inference(image_np, detections)
-    print("End of Function")
+    print(f"{received_image} INFERENCED COMPLETED")
 
 
-# Socket Function
-def connect():
-    global sock
-    global connbuf
-    # Initial connection
-    print("Attempt connection")
+# Load pipeline config and build a detection model
+configs = config_util.get_configs_from_pipeline_file(CONFIG_PATH)
+detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+# Restore checkpoint
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore(os.path.join(CHECKPOINT_PATH, 'ckpt-23')).expect_partial()
+
+# client socket connection to rpi server
+try:
+    # connect to server
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
-    connbuf = buffer.Buffer(sock)
-    # sock.settimeout(360) #for 6 min?
     print(f"connected to {sock.getpeername()}")
-
-
-def check_connection():
-    data = sock.recv(1024)
-    if len(data) == 0:
-        connect()
-
-
-def send_message(message):
-    sock.send(message.encode("utf-8"))
-
-
-def receive_info():
-    received = sock.recv(1024).decode('utf-8')
-    print("received: " + received)
-    # filename, filesize = received.split('|')
-    file_name = connbuf.get_utf8()
-    file_size = int(connbuf.get_utf8())
-    return file_name, file_size
-
-
-# def write_file(file_name):
-#     with open(file_name, 'wb') as f:
-#         while True:
-#             bytes_read = sock.recv(1024)
-#             if not bytes_read:
-#                 break
-#             f.write(bytes_read)
-#         print("File written")
-
-def write_file(file_name, file_size):
-    with open(file_name, 'wb') as f:
-        remaining = file_size
-        while remaining:
-            chunk_size = 1024 if remaining >= 1024 else remaining
-            chunk = connbuf.get_bytes(chunk_size)
-            if not chunk:
-                break
-            f.write(chunk)
-            remaining -= len(chunk)
-        if remaining:
-            print('File incomplete.  Missing', remaining, 'bytes.')
-        else:
-            print('File received successfully.')
-
-try:
-    connect()
-    # Send message to trigger taking of images (this should be send by algo in future)
-    send_message("CV|TAKIMG")
-
-    while True:
-        image_count = 0
-        # Check for connection -> no: reconnect, yes: listen for message
-        check_connection()
-
-        # Received message -> read file to be passed into detection model
-        file_name, file_size = receive_info()
-        file_name = f'{IMAGES_PATH}/{file_name}.jpg'  # see if sending over have jpg
-        write_file(file_name, file_size)
-
-        # Pass image into the detection model
-        run_inference_for_single_image(file_name)
-        image_count += 1
-
-        # item = ["30_set_5_050.jpg", "40_set_2_028.jpg", "50_set_2_016.jpg"]
-        # for i in range(len(item)):
-        #     run_inference_for_single_image(IMAGES_PATH + "/" + item[i])
-        #     image_count += 1
-
+    with sock:
+        sbuf = buffer.Buffer(sock)
+        # once socket connected send command
+        sbuf.put_utf8("CV|TAKIMG")
+        print("cv takimg called")
+        # client receive file name and size first
+        file_name = sbuf.get_utf8()
+        file_size = int(sbuf.get_utf8())
+        print(f'{file_name} {file_size}')
+        # client receive file data
+        file_full_name = f'{RAW_IMAGES_PATH}/{file_name}.jpg'
+        with open(file_full_name, 'wb') as f:
+            remaining = file_size
+            while remaining:
+                chunk_size = 1024 if remaining >= 1024 else remaining
+                chunk = sbuf.get_bytes(chunk_size)
+                if not chunk: break
+                f.write(chunk)
+                remaining -= len(chunk)
+            if remaining:
+                print('File incomplete.  Missing', remaining, 'bytes.')
+            else:
+                print(f'File {file_name} received successfully.')
+        # once file is receive, spawn new process to run image inference
+        p = Process(target=run_inference_for_single_image, args=(file_full_name,)).start()
+        # run_inference_for_single_image(file_name)
 except KeyboardInterrupt:
     sock.close()
 except socket.timeout as e:
     print(e)
     sock.close()
 except Exception as e:
+    exception_type, exception_object, exception_traceback = sys.exc_info()
+    line_number = exception_traceback.tb_lineno
+    print("Exception type: ", exception_type)
+    print("Line number: ", line_number)
     print(f'ImageCV:{e}')
 
-    # new process to do detection
-    # p = Process(target=read_image, args=[file_name], daemon=True)
-    # p.start()
 
+# # Run without Socket
 # try:
-#     # Initial connection
-#     print("Attempt connection")
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     sock.connect(("192.168.10.1", 50001))
-#     print(f"connected to {sock.getpeername()}")
+#     # imgs = ["./images/test_estimate_set1_L_0.jpg",
+#     #         "./images/test_estimate_set1_L_1.jpg", "./images/test_estimate_set1_R_0.jpg",
+#     #         "./images/test_estimate_set1_R_1.jpg", "./images/test_estimate_set2_0.jpg"]
+#     imgs2 = ["./images/30_set_1_005.jpg", "./images/30_set_5_050.jpg",
+#              "./images/40_set_2_028.jpg", "./images/50_set_2_016.jpg"]
 #
-#     image_count = 0
+#     run_inference_for_single_image("./images/30_set_1_005.jpg")
 #
-#     # Send message to trigger taking of images
-#     send_message("CV|TAKIMG")
-
-#     # Received message -> read file to be passed into detection model
-#     file_name = f'received_data_{image_count}.jpg'
-#     receive_file_info()
-#     write_file()
-#
-# new process to do detection
-# p = Process(target=read_image, args=[file_name], daemon=True)
-# p.start()
-#
-#     print("Finish")
-# except KeyboardInterrupt:
-#     sock.close()
-# except socket.timeout as e:
-#     print(e)
-#     sock.close()
-# except Exception as e:
-#     print(f'Imagecv:{e}')
-
-# try:
-#     while True:
-#         image_count = 0
-#         item = ["30_set_5_050.jpg", "40_set_2_028.jpg", "50_set_2_016.jpg"]
-#
-#         for i in range(len(item)):
-#             run_inference_for_single_image(IMAGES_PATH + "/" + item[i])
-#             image_count += 1
+#     # for i in range(len(imgs2)):
+#     #     run_inference_for_single_image(imgs2[i])
+#     #     image_count += 1
 #
 # except Exception as e:
 #     print(f'ImageCV:{e}')
